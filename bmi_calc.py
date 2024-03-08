3993d98923f3fbad9b797186adc649100f820526
Stephen <stephenproctor291@gmail.com>
2024-03-07 23:06:25 -0600

    Code commenting, added more test cases

--- bmi_calc.py
+++ bmi_calc.py
@@ -1,46 +1,55 @@
 
-
+# convert height to inches
 def convert_to_inches(usr_height):
     
+    # split the input string into a list
     height_list = usr_height.split(" ")
 
+    # convert the list elements to integers
     num_ft = int(height_list[0])
     num_inches = int(height_list[1])
 
+    # calculate the total inches
     total_inches = (num_ft * 12) + num_inches
 
     return total_inches
 
-
+# convert weight to kg
 def metric_conversion_weight(usr_weight):
     num_kg = int(usr_weight) * 0.45
 
     return num_kg
 
-
+# convert height to meters
 def metric_conversion_height(usr_height):
     num_meters = int(usr_height) * 0.025
 
     return num_meters
 
+# square the height for BMI formula
 def meters_squared(usr_height):
     return usr_height ** 2
 
-
+# calculate BMI
 def bmi_conversion(usr_height, usr_weight):
 
+    # call function to convert weight to kg
     usr_weight_kg = metric_conversion_weight(usr_weight)
 
+    # call function to convert height to meters
     usr_height_meters = metric_conversion_height(usr_height)
 
+    # call function to square the height
     usr_height_meters = meters_squared(usr_height_meters)
     
+    # calculate BMI
     bmi = usr_weight_kg / usr_height_meters
 
     return bmi
 
+# categorize BMI
 def bmi_category(bmi):
-
+    # basic if-else statements to categorize BMI
     if (bmi < 18.5):
         print ("\nYour BMI is %.3f. Category: Underweight" % bmi)
         return "Underweight"
@@ -59,20 +68,26 @@ def bmi_category(bmi):
 
 
 def main():
-
+    # Introductory messages
     print ("Welcome to BMI Calculator")
+    # Instructions
     print ("Height should be entered in the form of 'ft in'. Ex: 5 11")
     print ("Weight should be entered in lbs\n")
 
+    # User input
     usr_height = input("Enter your height: ")
+    # Convert height to inches
     usr_height = convert_to_inches(usr_height)
 
+    # User input
     usr_weight = input("Enter your weight: ")
 
+    # Call BMI function based on gathered user input
     bmi = bmi_conversion(usr_height, usr_weight)
 
+    # Call BMI category function based on calculated BMI
     bmi_category(bmi)
 
-
+# Run the main function
 if __name__=="__main__":
     main()
--- test_bmi_calc.py
+++ test_bmi_calc.py
@@ -6,59 +6,73 @@ class TestBmiCalc:
 
     def test_convert_to_inches(self):
         # Test arbitrary heights
+        # Realistic values to ensure the function is working
         assert convert_to_inches("5 10") == 70
         assert convert_to_inches("5 5") == 65
         assert convert_to_inches("6 0") == 72
 
         # Test upper and lower boundaries
+        # Nobody is 0 inches tall, nobody is 12 feet tall
         assert convert_to_inches("0 0") == 0
         assert convert_to_inches("12 0") == 144
 
 
     def test_metric_conversion_weight(self):
         # Test arbitrary weights
+        # Realistic values to ensure the function is working
         assert metric_conversion_weight(150) == 67.5
         assert metric_conversion_weight(200) == 90
         assert metric_conversion_weight(100) == 45
 
         # Test upper and lower boundaries
+        # Nobody weighs 0 pounds, nobody weighs 1000 pounds
         assert metric_conversion_weight(0) == 0
         assert metric_conversion_weight(1000) == 450
 
 
     def test_metric_conversion_height(self):
         # Test arbitrary heights
+        # Realistic values to ensure the function is working
         assert metric_conversion_height(70) == 1.75
         assert metric_conversion_height(65) == 1.625
         assert metric_conversion_height(45) == 1.125
 
         # Test upper and lower boundaries
+        # Nobody is 0 inches tall, nobody is 1000 inches tall
         assert metric_conversion_height(0) == 0
         assert metric_conversion_height(1000) == 25
 
 
     def test_meters_squared(self):
         # Test arbitrary values
+        # Realistic values to ensure the function is working
         assert meters_squared(13) == 169
         assert meters_squared(16.5) == 272.25
         assert meters_squared(10) == 100
 
         # Test upper and lower boundaries
+        # Nobody is 0 meters tall, nobody is 100 meters tall
         assert meters_squared(0) == 0
         assert meters_squared(100) == 10000
 
 
     def test_bmi_conversion(self):
         # Test conversions
+        # Realistic values to ensure the function is working
         assert bmi_conversion(80, 130) == 14.625
         assert bmi_conversion(60, 100) == 20.0
         assert bmi_conversion(40, 75) == 33.75
 
+        # Test upper and lower boundaries
+        # Nobody is 10 inches tall and weighs 10 pounds
         assert bmi_conversion(10, 10) == 72.0
+        # Nobody is 1000 inches tall and weighs 1000 pounds
         assert bmi_conversion(1000, 1000) == 0.72
 
 
     def test_bmi_category(self):
+
+        # Utilize EPC boundary testing
 
         ## Test underweight subdomain
         # Test under point
